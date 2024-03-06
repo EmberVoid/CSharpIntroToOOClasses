95080c3aebbcb06f737deb7b9429c3a8c40354ae
Jean <jeancarlo199797@gmail.com>
2024-03-05 21:31:35 -0600

    First commit, middway of OO labs, starting Inheritance

---
+++ Checkerboard/Program.cs
@@ -0,0 +1,14 @@
+﻿using Boards;
+
+namespace Checkerboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boards.Board chessBoard = new() { Rows = 8, Columns = 8 };
+            // chessBoard.Setup();
+            chessBoard.Print();
+        }
+    }
+}
