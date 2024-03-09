7b5ad24ca3736d00eb25a9c0e751336fb946f325
Jean <jeancarlo199797@gmail.com>
2024-03-08 18:54:51 -0600

    Add foreach app

---
+++ Foreach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planets = new List<string>();
+            planets.AddRange("Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune,Pluto".Split(','));
+            planets.Sort();
+
+            foreach (string p in planets)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
